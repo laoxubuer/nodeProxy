4a9e9bb43eb81ad947f955cd37c1265407cf0806
boyn <boyn@boyndeMacBook-Pro.local>
2015-08-21 11:40:36 +0800

    [nodeproy]优化

--- routers/user/index.js
+++ routers/user/index.js
@@ -7,10 +7,13 @@ var express = require('express'),
 /**
  * 用户列表路由
  */
-router.get('/user/userlist', function(req, res){
-	var apiArr = ['/c','/b'];
+// router.get('/user/userlist', function(req, res){
+// 	var apiArr = ['/c','/b'];
 
-	proxy.buildReq(req, res, apiArr)
+// 	proxy.buildReq(req, res, apiArr)
+// })
+router.get('/user/userlist', function(req, res){
+	proxy.httpHanlder(req, res, '/c');
 })
 
 /**
